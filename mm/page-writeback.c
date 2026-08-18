6f05aeb3a8013d5c3eb01a1fd90c82d380b30530
Sara Sena <sarasena.adr@gmail.com>
2026-08-18 20:08:00 -0300

    WIP: transcribe mm/page-writeback.c

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -180,3 +180,232 @@ static void wb_min_max_ratio(struct bdi_writeback *wb,
         *maxp = max;
 }
 
+#else   /* CONFIG_CGROUP_WRITEBACK */
+
+#define GDTC_INIT(__wb)         .wb = (__wb),
+                                .wb_completions = &(__wb)->wb_completions
+#define GDTC_INIT_NO_WB
+#define MDTC_INIT(__wb, __gdtc)
+
+static bool mdtc_valid(struct dirty_throttle_control *dtc)
+{
+        return false;
+}
+
+static struct wb_domain *dtc_dom(struct dirty_throttle_control *dtc)
+{
+        return &global_wb_domain;
+}
+
+static struct dirty_throttle_control *mdtc_gdtc(struct dirty_throttle_control *mdtc)
+{
+        return NULL;
+}
+
+static struct fprop_local_percpu *wb_memcg_completions(struct bdi_writeback *wb)
+{
+        return NULL;
+}
+
+static void wb_min_max_ratio(struct bdi_writeback *wb,
+                             unsigned long *minp, unsigned long *maxp)
+{
+        *minp = wb->bdi->min_ratio;
+        *maxp = wb->bdi->max_ratio;
+}
+
+#endif /* CONFIG_CGROUP_WRITEBACK */
+
+/*
+ * In a memory zone, there is a certain amount of pages we consider
+ * available for the page cache, which is essentially the number of
+ * free and reclaimable pages, minus some zone reserves to protect
+ * lowmem and the ability to uphold the zone's watermarks without
+ * requiring writeback.
+ *
+ * This number of dirtyable pages is the base value of which the
+ * user-configurable dirty ratio is the effective number of pages that
+ * are allowed to be actually dirtied.  Per individual zone, or
+ * globally by using the sum of dirtyable pages over all zones.
+ * 
+ * Because the user is allowed to specify the dirty limit globally as
+ * absolute number of bytes, calculating the per-zone dirty limit can
+ * require translating the configured limit into a percentage of 
+ * global dirtyable memory first.
+ */
+
+/**
+ * node_dirtyable_memory - number of dirtyable pages in a node
+ * @pgdat: the node
+ * 
+ * Return: the node's number of pages potentially available for dirty
+ * page cache.  This is the base value for the per-node dirty limits.
+ */
+static unsigned long node_dirtyable_memory(struct pglist_data *pgdat)
+{
+        unsigned long nr_pages = 0;
+        int z;
+
+        for (z = 0; z < MAX_NR_ZONES; z++) {
+                struct zone *zone = pgdat->node_zones + z;
+
+                if (!populated_zone(zone))
+                        continue;
+
+                nr_pages += zone_page_state(zone, NR_FREE_PAGES);
+        }
+
+        /*
+         * Pages reserved for the kernel should not be considered
+         * dirtyable, to prevent a situation where reclaim has to
+         * clean pages in order to balance the zones.
+         */
+        nr_pages -= min(nr_pages, pgdat->totalreserve_pages);
+
+        nr_pages += node_page_state(pgdat, NR_INACTIVE_FILE);
+        nr_pages += node_page_state(pgdat, NR_ACTIVE_FILE);
+
+        return nr_pages;
+}
+
+static unsigned long highmem_dirtyable_memory(unsigned long total)
+{
+#ifdef CONFIG_HIGHMEM
+        int node;
+        unsigned long x = 0;
+        int i;
+
+        for_each_node_state(node, N_HIGH_MEMORY) {
+                for (i = ZONE_NORMAL + 1; i < MAX_NR_ZONES; i++) {
+                        struct zone *z;
+                        unsigned long nr_pages;
+
+                        if (!is_highmem_idx(i))
+                                continue;
+
+                        z = &NODE_DATA(node)->node_zones[i];
+                        if (!populated_zone(z))
+                                continue;
+
+                        nr_pages = zone_page_state(z, NR_FREE_PAGES);
+                        /* watch for underflows */
+                        nr_pages -= min(nr_pages, high_wmark_pages(z));
+                        nr_pages += zone_page_state(z, NR_ZONE_INACTIVE_FILE);
+                        nr_pages += zone_page_state(z, NR_ZONE_ACTIVE_FILE);
+                        x += nr_pages;
+                }
+        }
+
+        /*
+         * Make sure that the number of highmem page is never larger
+         * than the number of the total dirtyable memory. This can only
+         * occur in very strange VM situations but we want to make sure
+         * that this does not occur.
+         */
+        return min(x, total);
+#else
+        return 0;
+#endif
+}
+
+/**
+ * global_dirtyable_memory - number of globally dirtyable pages
+ *
+ * Return: the global number of pages potentially available for dirty
+ * page cache.  This is the base value for the global dirty limits.
+ */
+static unsgined long global_dirtyable_memory(void)
+{
+        unsigned long x;
+
+        x = global_zone_page_state(NR_FREE_PAGES);
+        /*
+         * Pages reserved for the kernel should not be considered
+         * dirtyable, to prevent a situation where reclaim has to
+         * clean pages in order to balance the zones.
+         */
+        x -= min(x, totalreserve_pages);
+
+        x += global_node_page_state(NR_INACTIVE_FILE);
+        x += global_node_page_state(NR_ACTIVE_FILE);
+
+        if (!vm_highmem_is_dirtyable)
+                x -= highmem_dirtyable_memory(x);
+
+        return x + 1;   /* Ensure that we never return 0 */
+}
+
+/**
+ * domain_dirty_limits - calculate thresh and bg_thresh for a wb_domain
+ * @dtc: dirty_throttle_control of interest
+ *
+ * Calculate @dtc->thresh and ->bg_thresh considering
+ * vm_dirty_{bytes|ratio} and dirty_background_{bytes|ratio}.  The caller
+ * must ensure that @dtc->avail is set before calling this function.  The
+ * dirty limits will be lifted by 1/4 for real-time tasks.
+ */
+static void domain_dirty_limits(struct dirty_throttle_control *dtc)
+{
+        const unsigned long available_memory = dtc->avail;
+        struct dirty_throttle_control * gdtc = mdtc_gdtc(dtc);
+        unsigned long bytes = vm_dirty_bytes;
+        unsigned long bg_bytes = dirty_background_bytes;
+        /* convert ratios to per-PAGE_SIZE for higher precision */
+        unsigned long ratio = (vm_dirty_ratio * PAGE_SIZE) / 100;
+        unsigned long bg_ratio = (dirty_background_ratio * PAGE_SIZE) / 100;
+        unsigned long thresh;
+        unsigned long bg_thresh;
+        struct task_struct *tsk;
+
+        /* gdtc is !NULL iff @dtc is for memcg domain */
+        if (gdtc) {
+                unsigned long global_avail = gdtc->avail;
+
+                /*
+                 * The byte settings can't be applied directly to memcg
+                 * domains.  Convert them to ratios by scaling against
+                 * globally available memory.  As the ratios are in
+                 * per-PAGE_SIZE, they can be obtained by diving bytes by
+                 * number of pages.
+                 */
+                if (bytes)
+                        ratio = min(DIV_ROUND_UP(bytes, global_avail),
+                                    PAGE_SIZE);
+                if (bg_bytes)
+                        bg_ratio = min(DIV_ROUND_UP(bg_bytes, global_avail),
+                                       PAGE_SIZE);
+                bytes = bg_bytes = 0;
+        }
+
+        if (bytes)
+                thresh = DIV_ROUND_UP(bytes, PAGE_SIZE);
+        else
+                thresh = (ratio * available_memory) / PAGE_SIZE;
+
+        if (bg_bytes)
+                bg_thresh = DIV_ROUND_UP(bg_bytes, PAGE_SIZE);
+        else
+                bg_thresh = (bg_ratio * available_memory) / PAGE_SIZE;
+
+        tsk = current;
+        if (rt_or_dl_task(tsk)) {
+                bg_thresh += bg_thresh / 4 + global_wb_domain.dirty_limit / 32;
+                thresh += thresh / 4 + global_wb_domain.dirty_limit / 32;
+        }
+        /*
+         * Dirty throttling logic assumes the limits in page units fit into
+         * 32-bits. This gives 16TB dirty limits max which is hopefully enough.
+         */
+        if (thresh > UINT_MAX)
+                thresh = UINT_MAX;
+        /* This makes sure bg_thresh is within 32-bits as well */
+        if (bg_thresh >= thresh)
+                bg_thresh = thresh / 2;
+        dtc->thresh = thresh;
+        dtc->bg_thresh = bg_thresh;
+
+        /* we should eventually report the domain in the TP */
+        if (!gdtc)
+                trace_global_dirty_state(bg_thresh, thresh);
+}
+
